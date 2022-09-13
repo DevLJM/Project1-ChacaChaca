4e0d55fe0c1b17462a1a0024798dacfdc0183c7b
khj <coding22526@gmail.com>
2022-09-13 16:18:46 +0900

    이용후기 게시판 작업

---
+++ src/com/chaca/review/action/ActionForward.java
@@ -0,0 +1,25 @@
+package com.chaca.review.action;
+
+public class ActionForward {
+	// 페이지 이동을 위한 정보를 저장하는 객체 (통행권)
+	private String path;
+	private boolean isRedirect;
+
+	// get set
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public boolean isRedirect() {
+		return isRedirect;
+	}
+
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
+	}
+
+}
